2d0c8fb7c0bf04195c35c3dc55dc63f051b4c2a3
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-08-27 22:14:47 +0900

    test: add test about number matcher

--- test/matcher.test.ts
+++ test/matcher.test.ts
@@ -29,7 +29,13 @@ describe('matchers', () => {
   expect(array).toIncludes('ping');
   expect(array).toContain('ping');
 
-  // todo: add number functions
+  const number = 2 + 2;
+
+  expect(number).toBeGreaterThan(3);
+  expect(number).toBeGreaterThanOrEqual(4);
+
+  expect(number).toBeLessThan(5);
+  expect(number).toBeLessThanOrEqual(4);
 
   // not.not
   // expect().not.not; => error
