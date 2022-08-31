d253fe1b4563e0992456267991914b6c8baca336
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-08-31 23:13:50 +0900

    fix: fixed await/async bugs for `setup`

--- src/setup/all/setup-all.ts
+++ src/setup/all/setup-all.ts
@@ -1,20 +1,17 @@
-/* eslint-disable no-unused-vars */
-import { SetupDone } from '../setup';
-
 function setup() {
   if (!global.estuaire_all_before) global.estuaire_all_before = null;
   if (!global.estuaire_all_after) global.estuaire_all_after = null;
 }
 
-function beforeAll(fn: (done: SetupDone) => Promise<void> | void) {
+function beforeAll(fn: () => Promise<void> | void) {
   setup();
 
   global.estuaire_all_before = fn;
 }
-function afterAll(fn: (done: SetupDone) => Promise<void> | void) {
+function afterAll(fn: () => Promise<void> | void) {
   setup();
 
-  global.global.estuaire_all_after = fn;
+  global.estuaire_all_after = fn;
 }
 
 export { beforeAll, afterAll };
--- src/setup/each/setup-each.ts
+++ src/setup/each/setup-each.ts
@@ -1,20 +1,17 @@
-/* eslint-disable no-unused-vars */
-import { SetupDone } from '../setup';
-
 function setup() {
   if (!global.estuaire_each_before) global.estuaire_each_before = null;
   if (!global.estuaire_each_after) global.estuaire_each_after = null;
 }
 
-function beforeEach(fn: (done: SetupDone) => Promise<void> | void) {
+function beforeEach(fn: () => Promise<void> | void) {
   setup();
 
   global.estuaire_each_before = fn;
 }
-function afterEach(fn: (done: SetupDone) => Promise<void> | void) {
+function afterEach(fn: () => Promise<void> | void) {
   setup();
 
-  global.global.estuaire_each_after = fn;
+  global.estuaire_each_after = fn;
 }
 
 export { beforeEach, afterEach };
