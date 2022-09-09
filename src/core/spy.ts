b005315ee812d32c4a32adcd5bdca4936c69e6a8
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-09-09 23:09:25 +0900

    chore: remove unused file

--- src/core/spy.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
-
-export default function spy() {
-  global.estuaire = {
-    version: JSON.parse(readFileSync(join(__dirname, '../package.json')).toString()).version,
-  };
-}
